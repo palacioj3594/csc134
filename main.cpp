852a8d43bda1a7f9a0ef629703da51a910253991
palacioj3594 <palacioj3594@student.faytechcc.edu>
2024-08-26 11:29:29 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// CSC 134
+// M1LAB - Apple Orchard
+// Julian Palacio
+// 8/26/24
+#include <iostream>
+using namespace std;
+
+int main() { 
+
+  // Set up variables
+  string name = "Null";
+  int num_apples = 10;
+  double price_per_apple = 0.25;
+
+  // Get user input
+  cout << "What is your name?\n";
+  cin >> name;
+  cout << "-------------------------\n";
+  
+  // Say hello
+  cout << "Welcome to the "; 
+  cout << name << " orchard!\n";
+  cout << "Each apple costs $" << price_per_apple << ".\n";
+  
+  // Ask some questions
+  cout << "How many apples would you like to buy?\n";
+  cin >> num_apples;
+  
+  // Give the answers
+  cout << "You are buying  " << num_apples << " apples.\n";
+
+  // Calculate the total
+  double total = num_apples * price_per_apple;
+  cout << "Your total is $" << total << ".\n";
+  
+}
