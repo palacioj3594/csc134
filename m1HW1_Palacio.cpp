792ee327cbb422ba2f7093a9e075353d762490ec
palacioj3594 <palacioj3594@student.faytechcc.edu>
2024-08-28 15:34:02 +0000

    Made a new file

---
+++ m1HW1_Palacio.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M1HW1 - Movie Talk
+// Julian Palacio
+// 8/28/24
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Set the variables
+    string movie_name = "The Bee Movie";
+    int year_made = 2007;
+    string money_made = "$293.51 million USD";
+
+    // Start Speaking
+    cout << movie_name << " was a movie made in " << year_made << " and made " << money_made << " when it released.";
+}
