4af9e7ac268471773f19a9847132195f9b4ad992
palacioj3594 <palacioj3594@student.faytechcc.edu>
2024-09-16 14:47:10 +0000

    Made progran + finished it

--- m3/m3t1_Palacio.cpp
+++ m3/m3t1_Palacio.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 //CSC 134
-//m3t1-
+//m3t1-Area of Two Rectangles
 //Julian Palacio
 //9/16/24
 
 int main() {
-    //set up variables
-    cout <<"M3T1\n";
+
+    // Set up variables
+    double width1, width2, length1, length2;
+    double area1, area2;
+
+    // Give output
+    cout << "M3T1 - Area of Two Rectangles\n";
+    cout << "What is the width of the first rectangle?\n";
+    cin >> width1;
+    cout << "What is the length for the first rectangle?\n";
+    cin >> length1;
+    cout << "What is the width for the second rectangle?\n";
+    cin >> width2;
+    cout << "What is the length for the second rectangle?\n";
+    cin >> length2;
+
+    // Calculate area
+    area1 = width1 * length1;
+    area2 = width2 * length2;
+
+    // Print answer
+    cout << "The first area is: " << area1 << ".\n";
+    cout << "The second area is: " << area2 << ".\n";
+
 }
