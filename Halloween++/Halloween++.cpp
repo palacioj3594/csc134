7e28e32179090ff58bd73daafc6db9187961e8ca
palacioj3594 <palacioj3594@student.faytechcc.edu>
2024-10-07 17:00:08 +0000

    Added random generation

--- Halloween++/Halloween++.cpp
+++ Halloween++/Halloween++.cpp
@@ -4,11 +4,14 @@
 //
 
 #include <iostream>
-#include "stdio.h" 
-// for getchar()
+#include "stdio.h" // for getchar()
+#include <random>
+
 using namespace std;
 
+void seed_generation(int,int);
 void move(int,int);
+void spawn(int, int);
 
 int main() {
 
@@ -27,39 +30,40 @@ int main() {
     }
 
     //Set variables
+    int spawn_check;
     int e_num_mod;
     int e_spawn_time_mod;
     if (difficulty == "EASY") {
 
         e_num_mod = 3;
-        e_spawn_time_mod = 0;
+        e_spawn_time_mod = 10;
 
     }
     else if (difficulty == "MEDIUM") {
 
         e_num_mod = 5;
-        e_spawn_time_mod = 1;
+        e_spawn_time_mod = 5;
 
     }
     else {
 
         e_num_mod = 7;
-        e_spawn_time_mod = 2;
+        e_spawn_time_mod = 0;
 
     }
 
     int MAX_ENEMIES = 5 + e_num_mod;
 
-    int respawn_time = 1 + e_spawn_time_mod;
+    int respawn_time = 5 + e_spawn_time_mod;
 
-    //cout << MAX_ENEMIES;
-    //cout << respawn_time;
+    //Set up movement
     char direction;
     char dir;
     int X_LOC;
     int Y_LOC;
     X_LOC = 0;
-    Y_LOC = 0; 
+    Y_LOC = 0;
+    spawn_check = 0;
     move(X_LOC, Y_LOC); 
     // start game loop
     while (true){
@@ -91,11 +95,23 @@ int main() {
         Y_LOC = 0;
     }
     move(X_LOC, Y_LOC);
+    //Enemy spawn check
+    if (respawn_time == 0) {
+        if (spawn_check == 0) {
+            seed_generation(X_LOC, Y_LOC);
+            spawn_check++;
+        }
+        spawn(X_LOC, Y_LOC);
+        respawn_time = e_spawn_time_mod + 5;
+    }
+    else {
+        respawn_time--;
+    }
     }// end loop
 }
 
+//For grid building
 void move(int X_LOC, int Y_LOC) {
-    //int X_LOC, Y_LOC;
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             if (X_LOC == j && Y_LOC == i) {
@@ -107,4 +123,30 @@ void move(int X_LOC, int Y_LOC) {
         }
         cout << endl;
     }
+}
+
+//For enemy spawning
+void spawn(int X_LOC, int Y_LOC) {
+    int E_X_LOC, E_Y_LOC;
+    E_X_LOC = rand() % 9;
+    if (E_X_LOC == X_LOC) {
+        while (E_X_LOC == X_LOC) {
+            E_X_LOC = rand() % 9;
+        }
+    }
+    E_Y_LOC = rand() % 9;
+    if (E_Y_LOC == Y_LOC) {
+        while (E_Y_LOC == Y_LOC) {
+            E_Y_LOC = rand() % 9;
+        }
+    }
+    cout << E_X_LOC;
+    cout << E_Y_LOC;
+}
+
+void seed_generation(int X_LOC, int Y_LOC) {
+    int seed;
+    seed = X_LOC * 10 + Y_LOC;
+    std::mt19937 gen(seed); // Got this from AI
+    
 }
