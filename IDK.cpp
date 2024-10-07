e6e087b7539a536a91ca606b98c9ca35e5bae532
palacioj3594 <palacioj3594@student.faytechcc.edu>
2024-10-07 14:28:38 +0000

    idk

---
+++ IDK.cpp
@@ -0,0 +1,18 @@
+//IDK
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int beer;
+    beer = 100;
+    while (beer >1){
+        cout << beer << "bottles of beer on the wall, " << beer << "bottles of beer." << endl;
+        beer--;
+        cout << "You take one down, pass it around, " << beer << " bottles of beer on the wall." << endl;
+    }
+     cout << beer << "bottle of beer on the wall, " << beer << "bottle of beer." << endl;
+     beer--;
+     cout << "You take one down, pass it around, " << beer << " bottles of beer on the wall." << endl;
+
+}
