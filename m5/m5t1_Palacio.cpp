b4bd53a99be8ffacfd6c4b8b7c6de6fe6d86782c
palacioj3594 <palacioj3594@student.faytechcc.edu>
2024-10-28 14:48:41 +0000

    added more things + numbers

--- m5/m5t1_Palacio.cpp
+++ m5/m5t1_Palacio.cpp
@@ -8,15 +8,32 @@ using namespace std;
 
 //Function prototypes (short version)
 void say_hello();
+int give_the_answer();
+int double_an_int(int number);
 
 //Main
 int main() {
     cout << "Hello from main!" << endl;
     say_hello();
+    cout << "The magic number is: ";
+    int number = give_the_answer();
+    cout << number << endl;
+    int twotimes = double_an_int(number);
+    cout << "Double that is: " << twotimes << endl;
     return 0;
 }
 
 //Function definitions
 void say_hello() {
     cout << "Hello from say_hello!" << endl;
+}
+
+int give_the_answer() {
+    float answer = 42;
+    return answer;
+}
+
+int double_an_int(int number) {
+    int twotimes = number * 2;
+    return twotimes;
 }
