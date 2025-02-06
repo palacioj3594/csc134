e9f6d4801ea04a790ad2b319045d6ddac3c12119
palacioj3594 <palacioj3594@student.faytechcc.edu>
2025-02-06 18:31:05 +0000

    check in for map

--- Final Project/Project_v2_Palacio.cpp
+++ Final Project/Project_v2_Palacio.cpp
@@ -30,7 +30,7 @@ typedef struct Weapon {
 typedef struct Player {
     int level;
     int strength;
-    int agility;
+    int dexterity;
     int speed;
     int luck;
     int hp;
@@ -38,6 +38,7 @@ typedef struct Player {
     string name;
     Armor armor;
     Weapon weapon;
+    int x, y;
 } Player;
 
 typedef struct Ally1 {
@@ -67,8 +68,8 @@ typedef struct Ally2 {
 } Ally2;
 
 typedef struct Tile {
-    bool level;
     bool traversable = true;
+    bool event_check = false;
     int event = 0;
 } Tile;
 
@@ -95,23 +96,29 @@ const int SLIME = 1;
 const int SKELETON = 2;
 const int FLY = 3;
 
+
+const int WIDTH = 11;
+const int HEIGHT = 11;
+
 vector<Enemy> enemyPool;
 
 //Functions
-void save(const Player& player, const Armor& armor, const Weapon& weapon, const string&);
-void load(Player& player, Armor& armor, Weapon& weapon, const string&);
+void save(const Player& player, const string&);
+void load(Player& player, const string&);
 void newGame(Player player);
 void draw(string);
-void Beginning_Town1(Player player);
+void gameState(Player& player);
 void Grasslands1(bool, Player player);
 void fight(bool, Area area, Enemy enemy, Player player);
 Enemy create_enemy(int);
 void fight_screen(Player player, Enemy enemy, vector<Enemy> fight_array, bool);
+void itemMenu(Player player);
 int damageCalc(vector<Enemy> fight_array, int, Player player);
 void getWeapon(Player& player);
+void drawMap(Player player);
 
 int main()  {
-    string filename = "savedata";
+    string filename;
     Player player;
     Armor armor;
     Weapon weapon;
@@ -138,53 +145,59 @@ int main()  {
         getline(cin, player.name);
         player.level = 1;
         player.strength = 10;
-        player.agility = 10;
+        player.dexterity = 10;
         player.speed = 10;
         player.luck = 10;
         player.hp = 10;
         player.mp = 10;
         player.armor.name = "Tunic";
         player.weapon.name = "Stick";
+        filename = player.name;
+        player.x = 6;
+        player.y = 6;
         getWeapon(player);
-        save(player, armor, weapon, filename);
+        save(player, filename);
         newGame(player);
     }
     else if(main_menu_choice == "LOAD GAME") {
-        load(player, armor, weapon, filename);
+        load(player, filename);
     }
-    save(player, armor, weapon, filename);
 }
 
-void save(const Player& player, const Armor& armor, const Weapon& weapon, const string& filename) {
+void save(const Player& player, const string& filename) {
     ofstream outFile(filename);
     if (outFile.is_open()) {
         outFile << player.name << endl;
         outFile << player.level << endl;
         outFile << player.strength << endl;
-        outFile << player.agility << endl;
+        outFile << player.dexterity << endl;
         outFile << player.speed << endl;
         outFile << player.luck << endl;
         outFile << player.hp << endl;
         outFile << player.mp << endl;
         outFile << player.armor.name << endl;
         outFile << player.weapon.name << endl;
+        outFile << player.x << endl;
+        outFile << player.y << endl;
     }
     outFile.close();
 }
 
-void load(Player& player, Armor& armor, Weapon& weapon, const string& filename) {
+void load(Player& player, const string& filename) {
     ifstream inFile(filename);
     if (inFile.is_open()) {
         inFile >> player.name;
         inFile >> player.level;
         inFile >> player.strength;
-        inFile >> player.agility;
+        inFile >> player.dexterity;
         inFile >> player.speed;
         inFile >> player.luck;
         inFile >> player.hp;
         inFile >> player.mp;
         inFile >> player.armor.name;
         inFile >> player.weapon.name;
+        inFile >> player.x;
+        inFile >> player.y;
     }
     inFile.close();
 }
@@ -207,6 +220,7 @@ void getWeapon(Player& player) {
             }
         }
     }
+    inFile.close();
 }
 
 void newGame(Player player) {
@@ -222,17 +236,31 @@ void newGame(Player player) {
     system("clear");
     cout << "Story stuff goes here. (WIP)" << endl;
     system("clear");
-    Beginning_Town1(player);
+    gameState(player);
 }
 
-void Beginning_Town1(Player player) {
+void gameState(Player& player) {
     Area area;
     area.name = "Beginning Town";
-
+    drawMap(player);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     //Town layout goes here, but I don't know how to make the town rn
-    system("clear");
+    cout << "What to do? (PROGRESS MENU)" << endl;
+    string choice;
+    getline (cin, choice);
+    while (choice != "MENU" && choice != "PROGRESS") {
+        cout << "INVALID INPUT" << endl;
+        cout << "What to do? (PROGRESS MENU)" << endl;
+        getline (cin, choice);
+    }
+    if (choice == "MENU") {
+        itemMenu(player);
+    }
+    else if (choice == "PROGRESS") {
+        system("clear");
     bool leavingtown = true;
     Grasslands1(leavingtown, player);
+    }
 }
 
 void Beginning_Town2() {
@@ -278,7 +306,7 @@ void fight(bool fight1, Area area, Enemy enemy, Player player) {
     for (const auto& enemy : fight_array) {
         Sorter.push_back({enemy.speed, enemy.name, false});
     }
-    TurnOrder Sorter1, Sorter2;
+    TurnOrder Sorter1;
     while (Sorter.size() > 0) {
         int index = 0;
         Sorter1 = Sorter[0];
@@ -339,9 +367,35 @@ void fight(bool fight1, Area area, Enemy enemy, Player player) {
             else if (fight_choice == "MAGIC") {
 
             }
+            else if (fight_choice == "ITEM") {
+
+            }
         }
         else {
             player.hp--;
+        }
+        if (fight_array.size() < 1) {
+            fight = false;
+            cout << "You Won!" << endl;
+            return;
+        }
+        if (player.hp <= 0) {
+            fight = false;
+            cout << "You have perished..." << endl;
+            cout << "Would you like to LOAD or QUIT?" << endl;
+            string lose_option;
+            getline(cin, lose_option);
+            while (lose_option != "LOAD" && lose_option != "QUIT") {
+                cout << "INVALID INPUT!" << endl;
+                cout << "Would you like to LOAD or QUIT?" << endl;
+            }
+            if (lose_option == "LOAD") {
+                string filename = player.name;
+                load(player, filename);
+            }
+            else if (lose_option == "QUIT") {
+                exit(EXIT_SUCCESS);
+            }
         }
     }
 }
@@ -355,6 +409,15 @@ int damageCalc(vector<Enemy> fight_array, int target_enemy, Player player) {
         }
         else {
             damage = ((player.weapon.damage * strMOD) / fight_array.at(target_enemy).defense) + ((rand() % player.weapon.quality) + 1);
+        }
+    }
+    else if (player.weapon.type == "fist") {
+        int fistMOD = player.level + ((player.strength + player.dexterity) / 2);
+        if (player.weapon.quality == 0) {
+            damage = (player.weapon.damage * fistMOD) / fight_array.at(target_enemy).defense;
+        }
+        else {
+            damage = ((player.weapon.damage * fistMOD) / fight_array.at(target_enemy).defense) + ((rand() % player.weapon.quality) + 1);
         }
     }
     return damage;
@@ -382,6 +445,26 @@ void fight_screen(Player player, Enemy enemy, vector<Enemy> fight_array, bool pl
     }
     else {
         cout << "Enemy Turn..." << endl;
+    }
+}
+
+void itemMenu(Player player) {
+    system("clear");
+    cout << "ITEM MENU" << endl;
+    cout << endl;
+    cout << player.name << endl;
+    cout << player.weapon.name << endl;
+    cout << endl;
+    cout << "Menu option (WEAPON, CLOSE): ";
+    string menu_choice1;
+    getline(cin, menu_choice1);
+    while (menu_choice1 != "WEAPON" && menu_choice1 != "CLOSE") {
+        cout << "INVALID OPTION" << endl;
+        cout << "Menu option (WEAPON, CLOSE): ";
+        getline(cin, menu_choice1);
+    }
+    if (menu_choice1 == "WEAPON") {
+
     }
 }
 
@@ -430,5 +513,40 @@ void draw(string screen) {
         cout << "----------------------------------------------------------------------------" << endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
         return;
+    }
+}
+
+void drawMap(Player player) {
+    system("clear");
+    int drawX = player.x;
+    int drawY = player.y;
+    int lineCheck = drawY - 5;
+    int charCheck1 = drawX - 5;
+    int charCheck2 = drawX + 5;
+    int charCheck3 = 1;
+    ifstream inFile("map.txt");
+    string line;
+    string x;
+    char del = ' ';
+    int current_line = 1;
+    for (int i = 1; i < drawY + 5; i++) {
+        int charCheck1 = drawX - 5;
+        int charCheck2 = drawX + 5;
+        int charCheck3 = 1;
+        while (getline(inFile, line)) { 
+            if(current_line == lineCheck) {
+                stringstream ss(line);
+                while (getline(ss,x,del)) {
+                    if (charCheck3 >= charCheck1 && charCheck3 <= charCheck2) {
+                        cout << x;
+                    }
+                    charCheck3++;
+                }
+            }
+            current_line++;
+            cout << current_line << charCheck1 << charCheck2 << charCheck3;
+            //std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        lineCheck++;
     }
 }
